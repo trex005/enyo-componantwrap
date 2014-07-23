e9fedf10835da53281618af8c42528471c464b35
trex005 <github@tryross.com>
2014-07-23 15:55:55 -0400

    Initial Creation.

---
+++ ComponentWrap.js
@@ -0,0 +1,17 @@
+enyo.kind({
+    name:'ComponentWrap'
+    constructor:function(props){
+        this.newComponents = props.components;
+        props.components = [];
+        this.inherited(arguments);
+    },
+    create:function(props){
+        this.inherited(arguments);
+        if(typeof this.$.wrapper !== 'undefined'
+           && typeof this.newComponents === 'object'){
+            for(var i = 0;i<this.newComponents.length;i++){
+                this.$.wrapper.createComponent(this.newComponents[i],{owner:this});
+            }
+        }
+    }
+});
