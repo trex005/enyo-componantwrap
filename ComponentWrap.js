4e1e564ddf6a9ae4d83469b97bbcf2776241454e
trex005 <github@tryross.com>
2014-08-07 11:37:46 -0400

    Updated owner
    Updated owner so wrapped components are always addressable from outside of kind

--- ComponentWrap.js
+++ ComponentWrap.js
@@ -2,6 +2,7 @@ enyo.kind({
     name:'ComponentWrap',
     constructor:function(props){
         this.newComponents = props.components;
+        this.newOwner = props.owner;
         props.components = [];
         this.inherited(arguments);
     },
@@ -10,7 +11,7 @@ enyo.kind({
         if(typeof this.$.wrapper !== 'undefined'
            && typeof this.newComponents === 'object'){
             for(var i = 0;i<this.newComponents.length;i++){
-                this.$.wrapper.createComponent(this.newComponents[i],{owner:this});
+                this.$.wrapper.createComponent(this.newComponents[i],{owner:this.newOwner});
             }
         }
     }
